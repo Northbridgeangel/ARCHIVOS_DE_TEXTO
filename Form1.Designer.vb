4c823d3081a4a8b2f5975f4163902fcd06c792ab
Northbridgeangel <67518040+Northbridgeangel@users.noreply.github.com>
2022-08-17 09:15:12 +0200

    Agregar archivos de proyecto.

---
+++ Form1.Designer.vb
@@ -0,0 +1,234 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.BTN_GRABAR = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.BTN_LEER_FICHERO = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.BTN_ORDEN_POR_APELLIDO = New System.Windows.Forms.Button()
+        Me.BTN_BUSCAR = New System.Windows.Forms.Button()
+        Me.BTN_LIMPIAR = New System.Windows.Forms.Button()
+        Me.BTN_BORRAR = New System.Windows.Forms.Button()
+        Me.BTN_GUARDAR_EDICION = New System.Windows.Forms.Button()
+        Me.BTN_EDITAR = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'BTN_GRABAR
+        '
+        Me.BTN_GRABAR.Location = New System.Drawing.Point(662, 12)
+        Me.BTN_GRABAR.Name = "BTN_GRABAR"
+        Me.BTN_GRABAR.Size = New System.Drawing.Size(126, 23)
+        Me.BTN_GRABAR.TabIndex = 0
+        Me.BTN_GRABAR.Text = "GRABA FICHERO"
+        Me.BTN_GRABAR.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(80, 97)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "NOMBRE:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(68, 169)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(69, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "APELLIDOS:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(108, 234)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(29, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "DNI:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(25, 299)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(112, 13)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "FECHA NACIMIENTO"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(152, 90)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(137, 20)
+        Me.TextBox1.TabIndex = 5
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(152, 162)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(137, 20)
+        Me.TextBox2.TabIndex = 6
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(152, 227)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(137, 20)
+        Me.TextBox3.TabIndex = 7
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(152, 292)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(137, 20)
+        Me.TextBox4.TabIndex = 8
+        '
+        'BTN_LEER_FICHERO
+        '
+        Me.BTN_LEER_FICHERO.Location = New System.Drawing.Point(662, 41)
+        Me.BTN_LEER_FICHERO.Name = "BTN_LEER_FICHERO"
+        Me.BTN_LEER_FICHERO.Size = New System.Drawing.Size(126, 23)
+        Me.BTN_LEER_FICHERO.TabIndex = 9
+        Me.BTN_LEER_FICHERO.Text = "LEER FICHERO"
+        Me.BTN_LEER_FICHERO.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(350, 87)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(287, 225)
+        Me.ListBox1.TabIndex = 10
+        '
+        'BTN_ORDEN_POR_APELLIDO
+        '
+        Me.BTN_ORDEN_POR_APELLIDO.Location = New System.Drawing.Point(662, 70)
+        Me.BTN_ORDEN_POR_APELLIDO.Name = "BTN_ORDEN_POR_APELLIDO"
+        Me.BTN_ORDEN_POR_APELLIDO.Size = New System.Drawing.Size(126, 23)
+        Me.BTN_ORDEN_POR_APELLIDO.TabIndex = 11
+        Me.BTN_ORDEN_POR_APELLIDO.Text = "POR APELLIDO"
+        Me.BTN_ORDEN_POR_APELLIDO.UseVisualStyleBackColor = True
+        '
+        'BTN_BUSCAR
+        '
+        Me.BTN_BUSCAR.Location = New System.Drawing.Point(662, 99)
+        Me.BTN_BUSCAR.Name = "BTN_BUSCAR"
+        Me.BTN_BUSCAR.Size = New System.Drawing.Size(126, 23)
+        Me.BTN_BUSCAR.TabIndex = 13
+        Me.BTN_BUSCAR.Text = "BUSCAR"
+        Me.BTN_BUSCAR.UseVisualStyleBackColor = True
+        '
+        'BTN_LIMPIAR
+        '
+        Me.BTN_LIMPIAR.Location = New System.Drawing.Point(662, 128)
+        Me.BTN_LIMPIAR.Name = "BTN_LIMPIAR"
+        Me.BTN_LIMPIAR.Size = New System.Drawing.Size(126, 23)
+        Me.BTN_LIMPIAR.TabIndex = 14
+        Me.BTN_LIMPIAR.Text = "LIMPIAR"
+        Me.BTN_LIMPIAR.UseVisualStyleBackColor = True
+        '
+        'BTN_BORRAR
+        '
+        Me.BTN_BORRAR.Location = New System.Drawing.Point(662, 159)
+        Me.BTN_BORRAR.Name = "BTN_BORRAR"
+        Me.BTN_BORRAR.Size = New System.Drawing.Size(126, 23)
+        Me.BTN_BORRAR.TabIndex = 15
+        Me.BTN_BORRAR.Text = "BORRAR"
+        Me.BTN_BORRAR.UseVisualStyleBackColor = True
+        '
+        'BTN_GUARDAR_EDICION
+        '
+        Me.BTN_GUARDAR_EDICION.Location = New System.Drawing.Point(350, 356)
+        Me.BTN_GUARDAR_EDICION.Name = "BTN_GUARDAR_EDICION"
+        Me.BTN_GUARDAR_EDICION.Size = New System.Drawing.Size(126, 23)
+        Me.BTN_GUARDAR_EDICION.TabIndex = 16
+        Me.BTN_GUARDAR_EDICION.Text = "GUARDAR EDICIÓN"
+        Me.BTN_GUARDAR_EDICION.UseVisualStyleBackColor = True
+        '
+        'BTN_EDITAR
+        '
+        Me.BTN_EDITAR.Location = New System.Drawing.Point(511, 356)
+        Me.BTN_EDITAR.Name = "BTN_EDITAR"
+        Me.BTN_EDITAR.Size = New System.Drawing.Size(126, 23)
+        Me.BTN_EDITAR.TabIndex = 17
+        Me.BTN_EDITAR.Text = "EDITAR"
+        Me.BTN_EDITAR.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.BTN_EDITAR)
+        Me.Controls.Add(Me.BTN_GUARDAR_EDICION)
+        Me.Controls.Add(Me.BTN_BORRAR)
+        Me.Controls.Add(Me.BTN_LIMPIAR)
+        Me.Controls.Add(Me.BTN_BUSCAR)
+        Me.Controls.Add(Me.BTN_ORDEN_POR_APELLIDO)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.BTN_LEER_FICHERO)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.BTN_GRABAR)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents BTN_GRABAR As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents BTN_LEER_FICHERO As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents BTN_ORDEN_POR_APELLIDO As Button
+    Friend WithEvents BTN_BUSCAR As Button
+    Friend WithEvents BTN_LIMPIAR As Button
+    Friend WithEvents BTN_BORRAR As Button
+    Friend WithEvents BTN_GUARDAR_EDICION As Button
+    Friend WithEvents BTN_EDITAR As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,461 @@
+﻿Imports System.IO 'IMPORTO LA CLASE SYSTEM.IO
+
+Public Class Form1
+
+    'Public oSW As New StreamWriter("C:\Cristina\A_BTN_REGISTROS\Archivo_REGISTROS.CSV", True) 'SIN "(,TRUE)" DESTRUYE EL ARCHIVO (YA QUE CREARÍA UNO NUEVO POR CADA NUEVO REGISTRO)
+    'CON LA PROPIEDAD "(,TRUE)" AÑADIDA A STREAMWRITER, RENUEVA EL ARCHIVO QUE YA FUE CREADO
+
+
+    Public MATRIX(1000, 4) As String
+    Public D As Integer
+    Public F As Integer
+    Public CAPITALIZA, FECHABIEN As String
+
+
+    Private Sub BTN_GRABAR_Click(sender As Object, e As EventArgs) Handles BTN_GRABAR.Click
+        Dim oSW As New StreamWriter("C:\Cristina\A_BTN_REGISTROS\Archivo_REGISTROS.CSV", True)
+
+        Call CAPITALIZAR(TextBox1.Text)
+        TextBox1.Text = CAPITALIZA
+        Call CAPITALIZAR(TextBox2.Text)
+        TextBox2.Text = CAPITALIZA
+        Call CAPITALIZAR(TextBox3.Text)
+        TextBox3.Text = CAPITALIZA
+        Call FECHAR(TextBox4.Text, 10)
+        TextBox4.Text = FECHABIEN
+
+        Dim LINEA As String = TextBox1.Text & ";" & TextBox2.Text & ";" & TextBox3.Text & ";" & TextBox4.Text 'VBNEWLINE, CREA NUEVA LÍNEA (mete linea en blanco)
+        oSW.WriteLine(LINEA)
+        oSW.Flush()
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+        TextBox4.Text = ""
+
+        oSW.Close()
+    End Sub
+
+    Private Sub BTN_LEER_FICHERO_Click(sender As Object, e As EventArgs) Handles BTN_LEER_FICHERO.Click
+        ListBox1.Items.Clear()
+        Dim oSR As New StreamReader("C:\Cristina\A_BTN_REGISTROS\Archivo_REGISTROS.CSV")
+        Dim LINEA As String
+        Do
+            LINEA = oSR.ReadLine()
+            If Not LINEA Is Nothing Then ListBox1.Items.Add(LINEA)
+        Loop Until LINEA Is Nothing
+        oSR.Close()
+    End Sub
+
+
+    '****************************************************************************************
+    'BOTÓN BUSCAR 
+    '****************************************************************************************
+    Private Sub BTN_BUSCAR_Click(sender As Object, e As EventArgs) Handles BTN_BUSCAR.Click
+        Dim BUSCA As String
+        BUSCA = InputBox("¿QUE BUSCAMOS?", "BUSCAR EN FICHERO DE TEXTO")
+        If BUSCA = "" Then Exit Sub
+        CAPITALIZAR(BUSCA)
+        BUSCA = CAPITALIZA
+        Dim p1, p2, p3 As Integer
+        Dim c As Integer
+        Dim oSR As New StreamReader("C:\Cristina\A_BTN_REGISTROS\Archivo_REGISTROS.CSV")
+        Dim LINEA As String
+        Do
+            LINEA = oSR.ReadLine()
+            If Not LINEA Is Nothing Then
+                If InStr(LINEA, BUSCA) <> 0 Then
+                    p1 = InStr(LINEA, ";")
+                    p2 = InStr(p1 + 1, LINEA, ";")
+                    p3 = InStr(p2 + 1, LINEA, ";")
+                    TextBox1.Text = Mid(LINEA, 1, p1 - 1)
+                    TextBox2.Text = Mid(LINEA, p1 + 1, p2 - (p1 + 1))
+                    TextBox3.Text = Mid(LINEA, p2 + 1, p3 - (p2 + 1))
+                    TextBox4.Text = Mid(LINEA, p3 + 1, Len(LINEA) - p3)
+                End If
+            End If
+        Loop Until LINEA Is Nothing
+        oSR.Close()
+    End Sub
+
+
+
+
+    Private Sub BTN_ORDEN_POR_APELLIDO_Click(sender As Object, e As EventArgs) Handles BTN_ORDEN_POR_APELLIDO.Click
+        Dim p1, p2, p3 As Integer
+        Dim c As Integer
+        Dim oSR1 As New StreamReader("C:\Cristina\A_BTN_REGISTROS\Archivo_REGISTROS.CSV")
+        Dim LINEA As String
+        'AVERIGUAR CUANTOS REGISTROS HAY
+        Do
+            LINEA = oSR1.ReadLine()
+            If Not LINEA Is Nothing Then c = c + 1
+        Loop Until LINEA Is Nothing
+
+        '--------------------------------------------------------------------
+        'COMO YA SE CUANTOS HAY CREO LA MATRIZ DE ESE TAMAÑO
+        Dim MATRIZ(c, 4) As String
+        c = 0
+        oSR1.Close()
+
+        '----------------------------------------------------------------------
+        'LLENA LA MATRIZ CON LOS DATOS
+        Dim oSR As New StreamReader("C:\Cristina\A_BTN_REGISTROS\Archivo_REGISTROS.CSV")
+        Do
+            LINEA = oSR.ReadLine()
+            If Not LINEA Is Nothing Then
+                c = c + 1
+                p1 = InStr(LINEA, ";")
+                p2 = InStr(p1 + 1, LINEA, ";")
+                p3 = InStr(p2 + 1, LINEA, ";")
+                MATRIZ(c, 1) = Mid(LINEA, 1, p1 - 1)
+                MATRIZ(c, 2) = Mid(LINEA, p1 + 1, p2 - (p1 + 1))
+                MATRIZ(c, 3) = Mid(LINEA, p2 + 1, p3 - (p2 + 1))
+                MATRIZ(c, 4) = Mid(LINEA, p3 + 1, Len(LINEA) - p3)
+            End If
+        Loop Until LINEA Is Nothing
+        oSR.Close()
+
+
+        '-----------------------------------------------------------------------------
+        ' ORDENACION DE LA MATRIZ
+        Dim ORDEN(c, 4) As String
+        Dim POSI As Integer
+        Dim MINIMO As String
+        For I = 1 To c
+            MINIMO = "ZZZZZZZZZ"
+            POSI = 0
+            For X = 1 To c
+                If MATRIZ(X, 2) < MINIMO Then
+                    MINIMO = MATRIZ(X, 2)
+                    POSI = X
+                End If
+
+            Next
+
+            ORDEN(I, 2) = MINIMO
+            ORDEN(I, 1) = MATRIZ(POSI, 1)
+            ORDEN(I, 3) = MATRIZ(POSI, 3)
+            ORDEN(I, 4) = MATRIZ(POSI, 4)
+            MATRIZ(POSI, 2) = "ZZZZZZZZZZZ"
+        Next
+        '-------------------------------------------------------------
+        'CARGO ORDEN EN EL LISTBOX1
+        ListBox1.Items.Clear()
+        For I = 1 To c
+            ListBox1.Items.Add(ORDEN(I, 1) & ";" & ORDEN(I, 2) & ";" & ORDEN(I, 3) & ";" & ORDEN(I, 4))
+        Next
+
+    End Sub
+    Private Sub BTN_LIMPIAR_Click(sender As Object, e As EventArgs) Handles BTN_LIMPIAR.Click
+        ListBox1.Items.Clear()
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+        TextBox4.Text = ""
+    End Sub
+
+    '*******************************************************************************************************************
+    'BTN_BORRAR
+    '*******************************************************************************************************************
+    ' 1ª FASE AVERIGUAR CUANTOS REGISTROS HAY 
+    Private Sub BTN_BORRAR_Click(sender As Object, e As EventArgs) Handles BTN_BORRAR.Click
+        Dim oSR2 As New StreamReader("C:\Cristina\A_BTN_REGISTROS\Archivo_REGISTROS.CSV")
+        Dim Linea As String
+        Dim c As Integer
+        Dim D As Integer
+        Dim P1, P2, P3 As Integer
+
+        Do
+            Linea = oSR2.ReadLine()
+            If Not Linea Is Nothing Then c = c + 1
+        Loop Until Linea Is Nothing
+        '-------------------------------------------------------
+        'COMO YA SE CUANTOS HAY CREO LA MATRIZ DE ESE TAMAÑO
+        Dim MATRIZ(c, 4) As String
+        oSR2.Close()
+        D = c ' COMO LUEGO USO C PARA NO PERDER EL VALOR LO METO EN D
+        c = 0
+
+        '---------------------------------------------------------
+        ' LLENA LA MATRIZ CON LOS DATOS
+        Dim oSR3 As New StreamReader("C:\Cristina\A_BTN_REGISTROS\Archivo_REGISTROS.CSV")
+        Do
+            Linea = oSR3.ReadLine()
+            If Not Linea Is Nothing Then
+                c = c + 1
+                P1 = InStr(Linea, ";")
+                P2 = InStr(P1 + 1, Linea, ";")
+                P3 = InStr(P2 + 1, Linea, ";")
+                MATRIZ(c, 1) = Mid(Linea, 1, P1 - 1)
+                MATRIZ(c, 2) = Mid(Linea, P1 + 1, P2 - (P1 + 1))
+                MATRIZ(c, 3) = Mid(Linea, P2 + 1, P3 - (P2 + 1))
+                MATRIZ(c, 4) = Mid(Linea, P3 + 1, Len(Linea) - P3)
+            End If
+        Loop Until Linea Is Nothing
+        oSR3.Close()
+        '----------------------------------------------------------------------
+        'BORRAR 2ª FASE 
+        ' BUSCAR
+        c = 0
+        Dim BUSCA As String
+        BUSCA = InputBox("¿Que Buscamos?", "BUSCAR EN FICHERO DE TEXTO")
+        If BUSCA = "" Then Exit Sub
+        CAPITALIZAR(BUSCA)
+        BUSCA = CAPITALIZA
+
+        Dim oSR As New StreamReader("C:\Cristina\A_BTN_REGISTROS\Archivo_REGISTROS.CSV")
+        Do
+            Linea = oSR.ReadLine()
+            If Not Linea Is Nothing Then
+                c = c + 1
+                If InStr(Linea, BUSCA) <> 0 Then
+                    P1 = InStr(Linea, ";")
+                    P2 = InStr(P1 + 1, Linea, ";")
+                    P3 = InStr(P2 + 1, Linea, ";")
+                    TextBox1.Text = Mid(Linea, 1, P1 - 1)
+                    TextBox2.Text = Mid(Linea, P1 + 1, P2 - (P1 + 1))
+                    TextBox3.Text = Mid(Linea, P2 + 1, P3 - (P2 + 1))
+                    TextBox4.Text = Mid(Linea, P3 + 1, Len(Linea) - P3)
+                    MATRIZ(c, 1) = ""
+                    MATRIZ(c, 2) = ""
+                    MATRIZ(c, 3) = ""
+                    MATRIZ(c, 4) = ""
+                End If
+            End If
+        Loop Until Linea Is Nothing
+        oSR.Close()
+        ' ULTIMA FASE GRABAR LA MATRIZ EN EL FICHERO (SIN EL QUE BORRAMOS)
+        ' AQUI USO EL RECORDATORIO D PARA HACER EL BUCLE
+        Dim oSR4 As New StreamWriter("C:\Cristina\A_BTN_REGISTROS\Archivo_REGISTROS.CSV")
+        For I = 1 To D
+            If MATRIZ(I, 1) <> "" Then
+                Dim Linea1 As String = MATRIZ(I, 1) & ";" & MATRIZ(I, 2) & ";" & MATRIZ(I, 3) & ";" & MATRIZ(I, 4)
+                oSR4.WriteLine(Linea1)
+            End If
+        Next
+        oSR4.Close()
+
+    End Sub
+
+    '*************************************************************************************************************
+    'GUARDAR EDICION
+    Private Sub BTN_GUARDAR_EDICION_Click(sender As Object, e As EventArgs) Handles BTN_GUARDAR_EDICION.Click
+        Dim a, b, g As String
+        a = TextBox1.Text
+        CAPITALIZAR(a)
+        b = TextBox2.Text
+        CAPITALIZAR(b)
+        g = TextBox4.Text
+        FECHAR(g, 10)
+        MATRIX(F, 1) = TextBox1.Text
+        MATRIX(F, 2) = TextBox2.Text
+        MATRIX(F, 3) = TextBox3.Text
+        MATRIX(F, 4) = TextBox4.Text
+        '-------------------
+        ' ULTIMA FASE GRABAR LA MATRIZ EN EL FICHERO (SIN EL QUE BORRAMOS)
+        ' AQUI USO EL RECORDATORIO D PARA HACER EL BUCLE
+        Dim oSR3 As New StreamWriter("C:\Cristina\A_BTN_REGISTROS\Archivo_REGISTROS.CSV")
+        For I = 1 To D
+
+            Dim Linea1 As String = MATRIX(I, 1) & ";" & MATRIX(I, 2) & ";" & MATRIX(I, 3) & ";" & MATRIX(I, 4)
+            oSR3.WriteLine(Linea1)
+
+        Next
+        oSR3.Close()
+        '----------------------
+        BTN_EDITAR.Visible = True
+        BTN_GUARDAR_EDICION.Visible = False
+    End Sub
+
+    '*************************************************************************************************************
+    'EDITAR REGISTRO
+    Private Sub BTN_EDITAR_Click(sender As Object, e As EventArgs) Handles BTN_EDITAR.Click
+        Dim F As Integer
+        Dim D As Integer
+        Dim MATRIX(F, 4) As String
+        ' 1ª FASE AVERIGUAR CUANTOS REGISTROS HAY 
+        Dim oSR1 As New StreamReader("C:\Cristina\A_BTN_REGISTROS\Archivo_REGISTROS.CSV")
+        Dim Linea As String
+        Dim c As Integer
+
+        Dim P1, P2, P3 As Integer
+        Do
+            Linea = oSR1.ReadLine()
+            If Not Linea Is Nothing Then c = c + 1
+        Loop Until Linea Is Nothing
+        '-------------------------------------------------------
+        'COMO YA SE CUANTOS HAY CREO LA MATRIZ DE ESE TAMAÑO
+        ReDim MATRIX(c, 4)
+        oSR1.Close()
+        D = c ' COMO LUEGO USO C PARA NO PERDER EL VALOR LO METO EN D
+        c = 0
+        '---------------------------------------------------------
+        ' LLENA LA MATRIZ CON LOS DATOS
+        Dim oSR2 As New StreamReader("C:\Cristina\A_BTN_REGISTROS\Archivo_REGISTROS.CSV")
+        Do
+            Linea = oSR2.ReadLine()
+            If Not Linea Is Nothing Then
+                c = c + 1
+                P1 = InStr(Linea, ";")
+                P2 = InStr(P1 + 1, Linea, ";")
+                P3 = InStr(P2 + 1, Linea, ";")
+                MATRIX(c, 1) = Mid(Linea, 1, P1 - 1)
+                MATRIX(c, 2) = Mid(Linea, P1 + 1, P2 - (P1 + 1))
+                MATRIX(c, 3) = Mid(Linea, P2 + 1, P3 - (P2 + 1))
+                MATRIX(c, 4) = Mid(Linea, P3 + 1, Len(Linea) - P3)
+            End If
+        Loop Until Linea Is Nothing
+        oSR2.Close()
+        '----------------------------------------------------------------------
+        'BORRAR 2ª FASE 
+        ' BUSCAR
+        c = 0
+        Dim BUSCA As String
+        BUSCA = InputBox("¿QUE BUSCAMOS?", "BUSCAR EN FICHERO DE TEXTO")
+
+        CAPITALIZAR(BUSCA)
+        BUSCA = CAPITALIZA
+
+        Dim oSR As New StreamReader("C:\Cristina\A_BTN_REGISTROS\Archivo_REGISTROS.CSV")
+        Do
+            Linea = oSR.ReadLine()
+            If Not Linea Is Nothing Then
+                c = c + 1
+                If InStr(Linea, BUSCA) <> 0 Then
+                    P1 = InStr(Linea, ";")
+                    P2 = InStr(P1 + 1, Linea, ";")
+                    P3 = InStr(P2 + 1, Linea, ";")
+                    TextBox1.Text = Mid(Linea, 1, P1 - 1)
+                    TextBox2.Text = Mid(Linea, P1 + 1, P2 - (P1 + 1))
+                    TextBox3.Text = Mid(Linea, P2 + 1, P3 - (P2 + 1))
+                    TextBox4.Text = Mid(Linea, P3 + 1, Len(Linea) - P3)
+                    MATRIX(c, 1) = ""
+                    MATRIX(c, 2) = ""
+                    MATRIX(c, 3) = ""
+                    MATRIX(c, 4) = ""
+                    F = c ' PAN Y TOMATE PA QUE NO TE ESCAPES
+                End If
+            End If
+        Loop Until Linea Is Nothing
+        oSR.Close()
+        '-----------------------------------------------------
+        BTN_EDITAR.Visible = False
+        BTN_GUARDAR_EDICION.Visible = True
+    End Sub
+
+
+
+
+
+
+    '*********************************************************************************************************************
+    'SUB CAPITALIZAR Y SUB FECHAR
+    '*********************************************************************************************************************
+    '*******************************************************************************************************************************************************************************************
+    Sub CAPITALIZAR(DD As String)
+        '*******************************************************************
+        'PON EN MAYUSCULAS LA PRIMERA LETRA DEL TEXTO QUE LE ENVIEMOS A LA FUNCIÓN
+        '****      Y LO DEVUELVE EN LA VARIABLE PUBLIC CAPITALIZA
+        If IsNumeric(DD) Then
+            CAPITALIZA = ""
+            MsgBox("Datos Incorrectos",, "TONTERÍA INTRODUCIDA")
+            Exit Sub
+        End If
+
+        DD = Trim(DD)
+        Dim L As Char
+        Dim MIDE As Integer
+        'Dim RESULTADO As String
+        Dim flag As Integer
+        CAPITALIZA = ""
+
+        'RUTINA PARA LOS ESPACIOS
+
+        flag = 0
+
+        MIDE = Len(DD)
+
+        For I = 1 To MIDE
+            L = Mid(DD, I, 1)
+
+            If L = " " Then
+                flag = 1
+                GoTo SAL
+            End If
+
+            If I = 1 Or flag = 1 Then
+                L = UCase(L)
+                flag = 0
+            Else
+                L = LCase(L)
+            End If
+
+SAL:            'RESULTADO = RESULTADO & L
+            CAPITALIZA = CAPITALIZA & L
+        Next
+        'TextBox2.Text = RESULTADO
+    End Sub
+
+
+
+    '19/02/2020
+
+
+    Sub FECHAR(DD As String, NN As Integer)
+        Dim barra1, barra2 As Integer
+        Dim d, m, y As String
+        Dim x As Integer
+        Dim xx As String
+        Dim VERSIFECHA As Date
+
+
+
+        Try
+            VERSIFECHA = CDate(DD)
+            Exit Try
+        Catch ex As Exception
+            MsgBox("ESA MIERDA NO ES NI NUNCA SERÁ UNA FECHA")
+        End Try
+
+
+        If NN <> 10 And NN <> 8 Then
+            MsgBox("Parametro Tipo de Fecha erroneo, por favor mete un 8 o un 10")
+            Exit Sub
+        End If
+
+        ' donde estan las barras???
+        barra1 = InStr(DD, "/")
+        barra2 = InStr(barra1 + 1, DD, "/")
+        'barra1= 3(dd/m) y barra2= 6(m/yy), siendo la fecha= DD(dd/mm/yy)
+
+        If barra1 <> 0 And barra2 <> 0 Then ' el humano ha metido las barras de fecha
+            d = Mid(DD, 1, barra1 - 1)
+            m = Mid(DD, barra1 + 1, barra2 - (barra1 + 1))
+            'COMPROBAR QUE D Y M SEN RAZONABLES
+            y = Mid(DD, barra2 + 1, Len(DD) - (barra2))
+            ' ahora le ponemos lo que le falta si no ha puesto el cero delante
+            If Len(d) = 1 Then d = "0" & d
+            If Len(m) = 1 Then m = "0" & m
+            If NN = 10 And Len(y) = 2 Then ' houston tenemos un poblema
+                x = DateAndTime.Year(DateAndTime.Today)
+                xx = CStr(x)
+                xx = Mid(xx, 3, 2)
+                If CInt(y) > CInt(xx) Then
+                    y = "19" & y
+                Else
+                    y = "20" & y
+                End If
+            End If
+        End If
+        FECHABIEN = d & "/" & m & "/" & y
+    End Sub
+
+
+    '********************************************************************************************************************
+    '***********************************************************************************************************************************************************************************************
+
+
+
+
+
+End Class
